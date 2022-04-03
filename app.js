b342e4b6d881f2476cadaff1d27297f544a48a55
Parnell <psteide@ford.com>
2022-04-03 09:15:32 -0400

    First draft

---
+++ app.js
@@ -0,0 +1,32 @@
+let acceptingInput = true
+
+const screen = document.querySelector("#screen")
+const buttons = document.querySelectorAll(".buttons span")
+document.querySelectorAll(".operator")[1].textContent = "/"
+document.querySelectorAll(".operator")[2].textContent = "*"
+
+buttons.forEach(button => button.addEventListener("click", () => {
+    if(button.textContent !== "=" && acceptingInput){
+        screen.textContent += button.textContent
+    }if(button.textContent === "C" && acceptingInput){
+        screen.textContent = null
+    }
+    try{
+        if (button.textContent === "=" && acceptingInput){
+             if (screen.textContent.includes("/0")){
+                 screen.textContent = "error"
+        }
+        screen.textContent = eval(screen.textContent)
+     }
+ }
+     catch(error){
+         screen.textContent = "error"
+         acceptingInput = false
+         setTimeout(function () {
+             acceptingInput = true
+         screen.textContent = null
+         }, 2000)
+     }
+ }))
+     
+
