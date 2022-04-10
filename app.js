559ca43f0c7309e2e94b5e7a5671f777711d7b56
Parnell <psteide@ford.com>
2022-04-10 09:06:21 -0400

    Update formating/nested if statement

--- app.js
+++ app.js
@@ -1,32 +1,35 @@
-let acceptingInput = true
+let acceptingInput = true;
 
-const screen = document.querySelector("#screen")
-const buttons = document.querySelectorAll(".buttons span")
-document.querySelectorAll(".operator")[1].textContent = "/"
-document.querySelectorAll(".operator")[2].textContent = "*"
+const screen = document.querySelector("#screen");
+const buttons = document.querySelectorAll(".buttons span");
+document.querySelectorAll(".operator")[1].textContent = "/";
+document.querySelectorAll(".operator")[2].textContent = "*";
 
-buttons.forEach(button => button.addEventListener("click", () => {
-    if(button.textContent !== "=" && acceptingInput){
-        screen.textContent += button.textContent
-    }if(button.textContent === "C" && acceptingInput){
-        screen.textContent = null
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    if (button.textContent !== "=" && acceptingInput) {
+      screen.textContent += button.textContent;
     }
-    try{
-        if (button.textContent === "=" && acceptingInput){
-             if (screen.textContent.includes("/0")){
-                 screen.textContent = "error"
-        }
-        screen.textContent = eval(screen.textContent)
-     }
- }
-     catch(error){
-         screen.textContent = "error"
-         acceptingInput = false
-         setTimeout(function () {
-             acceptingInput = true
-         screen.textContent = null
-         }, 2000)
-     }
- }))
-     
-
+    if (button.textContent === "C" && acceptingInput) {
+      screen.textContent = null;
+    }
+    try {
+      if (
+        button.textContent === "=" &&
+        acceptingInput &&
+        screen.textContent.includes("/0")
+      ) {
+        screen.textContent = "error";
+      } else if (button.textContent === "=" && acceptingInput) {
+        screen.textContent = eval(screen.textContent);
+      }
+    } catch (error) {
+      screen.textContent = "error";
+      acceptingInput = false;
+      setTimeout(function () {
+        acceptingInput = true;
+        screen.textContent = null;
+      }, 2000);
+    }
+  })
+);
